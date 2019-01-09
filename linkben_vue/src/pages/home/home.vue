5315d0046ab9df191d1d73a5a9d218101ea21201
linzesen <linzesen@lvshou.com>
2019-01-09 17:58:53 +0800

    linkben_vue/src/pages/home/home.vue

--- linkben_vue/src/pages/home/home.vue
+++ linkben_vue/src/pages/home/home.vue
@@ -1,23 +1,13 @@
 <template>
-    <div>
-        <home-header></home-header>
-        <home-swiper :list="swiperList"></home-swiper>
-    </div>
     <div class="wrapper">
     
     </div>
 </template>
 
 <script>
-import HomeHeader from './components/Header'
-import HomeSwiper from './components/Swiper'
 import axios from 'axios'
 export default {
   name: 'Home',
-  components: {
-      HomeHeader,
-      HomeSwiper,
-  },
   data () {
     return {
      swiperList:[]
@@ -40,31 +30,10 @@ export default {
    this.getHomeInfo();
   },
   computed: {
-    
-import axios from 'axios'
-export default {
-  name: 'Home',
-  data () {
-    return {
-    }
-  },
-  mounted () {
-    axios.get('/zhihu/api/4/news/before/20190106')
-        .then(function(res){
-
-        });
-  },
-  computed: {
-    
   }
-  },
-  methods : {
-   }
 
 }
 </script>
 
 <style>
 </style>
-</script>
-
