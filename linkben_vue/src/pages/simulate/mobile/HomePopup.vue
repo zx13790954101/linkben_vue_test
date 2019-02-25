0cb4995a8c8db691475ca4ce3fa2b7db0b7d79bc
linzesen <linzesen@lvshou.com>
2019-02-25 18:14:36 +0800

    1

---
+++ linkben_vue/src/pages/simulate/mobile/HomePopup.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="homePopup">
+
+
+      <el-dialog  title=""  :visible.sync="dialogVisible" width="30%" >
+        <div class="content flex-c">
+            <div class="flex-item item">
+                <div class="img-r act-img">
+                   <img >
+                </div>
+            </div>
+        </div>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'homePopup',
+    data() {
+      return {
+        title: '首页的功能选择',
+        screenWidth: document.documentElement.clientWidth,
+        active: 0,
+        dialogVisible:false
+      }
+    },
+    props: {
+      imgStyleStatus: {}
+    },
+    created() {
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    },
+    computed: {
+
+    },
+
+  }
+</script>
+
+<style scoped lang="less">
+.homePopup{
+
+}
+
+</style>
