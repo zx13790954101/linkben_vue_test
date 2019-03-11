2ab8dedfad554e4df21e95fb8d9b42d556115b4e
linzesen <linzesen@lvshou.com>
2019-03-11 17:54:19 +0800

    1111

--- linkben_vue/gulpfile.js
+++ linkben_vue/gulpfile.js
@@ -1,3 +1,17 @@
 var gulp = require('gulp');
+var sftp = require('gulp-sftp-new');
+ 
 gulp.task('upload', function () {
+    return gulp.src('dist/**')
+        .pipe(sftp({
+          host: '47.93.198.213', //ip地址
+          user: 'linkben', //帐号
+          pass: "C7PaiFkfid", //密码
+          port: "21" ,
+          protocol: "sftp",
+          algorithms: {
+              serverHostKey: ['ssh-rsa', 'ssh-dss']
+            },
+          remotePath: '/www/wwwroot/linkben.com/linkben_test/public/dist', // 仅仅是结尾多了一个”/“，部署到服务器的路径
+        }));
 });
