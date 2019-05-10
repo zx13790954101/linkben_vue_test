bc3fc21b64a35bf0e092bc10a58ea289c6c0fd34
zx13790954101 <13790954101@163.com>
2019-05-10 18:13:00 +0800

    linkben_vue/build/scp2-test-dist.js

--- linkben_vue/build/scp2-test-dist.js
+++ linkben_vue/build/scp2-test-dist.js
@@ -1,8 +1,13 @@
 'use strict'
 var client = require('scp2');
-
+var lvShouConfig = require('../config/linzesen-config')
 // var random = Math.random()
 var scpConfig = {
+  "host": "47.93.198.213",
+  "username": "linkben",
+  "password": "C7PaiFkfid",
+  "port": "21",
+  "path": "/"
 };
 
 console.log("正在上传您的打包文件到测试环境："+ scpConfig.host+":"+ scpConfig.port  +" ，请稍后...")
@@ -17,19 +22,11 @@ console.log("上传中...")
 //   }
 // })
 
-
-// 部署上线
-client.scp('./dist/', {
-  host: "47.93.198.213",
-  username: "linkben",
-  password: "C7PaiFkfid",
-  port: "21",
-  path: "/",
-}, function (err) {
-if (err) {
-  console.log(err)
-} else {
-  console.log('文件上传完毕!\n')
-}
-
+client.scp('./dist/', scpConfig, function(err) {
+  if(!err){
+    console.log("\n")
+    console.log("========scp2工具上传完毕=============\n远端服务地址："+ scpConfig.host+":"+ scpConfig.port + "\n远端上传文件路径："+ scpConfig.path +"\n \n")
+  }else{
+    console.log("err",err)
+  }
 })
